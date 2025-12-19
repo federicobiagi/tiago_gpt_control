5e3b66e0475a1757878cf8aa8b209158cb49f791
federico <federico.biagi98@gmail.com>
2025-12-19 21:04:14 +0100

    Initial commit

---
+++ gpt_control/gpt_manager_node.py
@@ -0,0 +1,191 @@
+# coding=utf-8
+import os
+from pathlib import Path
+import argparse
+
+import re
+from tiago_arm_control_clean import TiagoControl
+import rclpy
+from rclpy.node import Node
+import openai
+import json
+
+
+class GPTManager(Node):
+    def __init__(self, api_key: str, model: str = "gpt-5-mini", temperature: float = 0.2, with_tools: bool = True):
+        super().__init__("gpt_manager")
+
+        if not api_key:
+            raise RuntimeError("OPENAI_API_KEY not found and --api_key not provided")
+
+        openai.api_key = api_key
+        self.model = model
+        self.temperature = temperature
+        self.with_tools = with_tools
+    
+        script_dir = Path(__file__).parent.resolve()
+
+        if self.with_tools:
+            sysprompt_path = script_dir / "system_prompt_clean.txt"
+            sysprompt = sysprompt_path.read_text(encoding="utf-8")
+            self.chat_history = [{"role": "system", "content": sysprompt}]
+        else:
+            sysprompt_path = script_dir / "system_prompt_no_tools.txt"
+            sysprompt = sysprompt_path.read_text(encoding="utf-8")
+            self.chat_history = [{"role": "system", "content": sysprompt}]
+
+        self.tiago = TiagoControl()   
+        
+
+        tools_path = script_dir / "tools.json"
+        tools_content = tools_path.read_text(encoding="utf-8")
+        self.tools = json.loads(tools_content)
+
+
+
+
+    def ask(self, question: str) -> str:
+        """Send a question to the model and return the answer."""
+        self.chat_history.append({"role": "user", "content": question})
+
+        if self.with_tools:
+            response = openai.chat.completions.create(
+                model=self.model,
+                messages=self.chat_history,
+                tools=self.tools,
+                tool_choice="auto",
+            )
+            msg = response.choices[0].message
+            if msg.tool_calls:
+                for tool_call in msg.tool_calls:
+                    tool_name = tool_call.function.name
+                    tool_args = json.loads(tool_call.function.arguments)
+                    self.get_logger().info(f"Chiamata tool: {tool_name} con argomenti {tool_args}")
+                    if tool_name == "move_distance":
+                        self.tiago.move_distance(**tool_args)
+                        answer = "Movimento completato di {} metri.".format(tool_args['distance'])
+                    elif tool_name == "rotate_angle":
+                        self.tiago.rotate_angle(**tool_args)
+                        answer = "Rotazione completata di {} radianti.".format(tool_args['angle'])
+                    elif tool_name == "move_arm":
+                        self.tiago.move_arm(**tool_args)
+                        answer = "Braccio mosso ai joint angle {}.".format(tool_args['joint_positions'])
+                    elif tool_name == "grasp_object_by_name_front":
+                        self.tiago.grasp_object_by_name_front(**tool_args)
+                        answer = "Afferrato l'oggetto {}.".format(tool_args['object_name'])
+                    elif tool_name == "open_gripper":
+                        self.tiago.open_gripper()
+                        answer = "Gripper aperto."
+                    elif tool_name == "move_gripper":
+                        self.tiago.move_gripper(**tool_args)
+                        answer = "Movimento del gripper completato."
+                    elif tool_name == "detach_object":
+                        self.tiago.detach_object(**tool_args)
+                        answer = "Oggetto rilasciato."
+                    elif tool_name == "get_entity_position":
+                        position = self.tiago.get_entity_position(**tool_args)
+                        answer = f"La posizione di {tool_args['name']} è {position}."
+                    elif tool_name == "get_object_position_in_base":
+                        position = self.tiago.get_object_position_in_base(**tool_args)
+                        answer = f"La posizione di {tool_args['object_name']} nel frame base è {position}."
+                    self.chat_history.append({"role": "assistant", "content": answer})
+
+                    
+            else:
+                answer = msg.content
+                self.chat_history.append({"role": "assistant", "content": answer})
+            return answer
+        else:
+            response = openai.chat.completions.create(
+                model=self.model,
+                messages=self.chat_history,
+            )
+            answer = response.choices[0].message.content
+            self.extract_python_code(answer)
+            f = open("./code.txt", "r")
+            linesofcode = f.readlines()
+            for code in linesofcode:  #esegui la lista di metodi che GPT ha prodotto
+                #print("Codice da eseguire: " + code)
+                code = code.replace('\n','')
+                if self.check_code_integrity(code):
+                    print("Step di codice da eseguire: ")
+                    print(code)
+                    exec('self.tiago.' + code.replace('\\',""))
+            self.chat_history.append({"role": "assistant", "content": answer})
+            return answer
+        
+    def check_code_integrity(self, code: str) -> bool:
+        forbidden_keywords = ["import", "open", "exec", "eval", "__", "os.", "sys.", "subprocess", "shutil"]
+        for keyword in forbidden_keywords:
+            if keyword in code:
+                self.get_logger().warning(f"Codice non sicuro rilevato: {keyword}")
+                return False
+        return True
+    
+    def extract_python_code(self,content):
+        code_block_regex = re.compile(r"```(.*?)```",re.DOTALL)  #prende il pezzo di codice che è specificato in "assistant" "content" tra 3 apici inversi
+        code_blocks = code_block_regex.findall(content) #trova quanto specificato secondo le regole definite prima con re.compile, cioè estrae il testo compreso tra ```
+        if code_blocks:
+            full_code = "\n".join(code_blocks) #aggrega le stringhe separandole con un 'a capo'
+
+            if full_code.startswith("python"):
+                full_code = full_code[7:]  #prende il codice dalla parola 'python' in poi, non compresa
+                full_code = full_code.replace("\\n", "\n")
+                open('./code.txt','w').write(full_code)
+
+            return full_code.rstrip()
+        else:
+            return None
+
+
+    def run(self):
+        print("Nodo GPT avviato. Comand TIAGO") 
+        print("Scrivi 'exit' o 'quit' per fermare.\n")
+
+        try:
+            while rclpy.ok():
+                try:
+                    user_input = input("User: ").strip()
+                except EOFError:
+                    break
+
+                if not user_input:
+                    continue
+
+                if user_input.lower() in ("exit", "quit"):
+                    break
+
+                answer = self.ask(user_input)
+                if answer and not 'python' in answer:
+                    print(f"Gpt Bot: {answer}\n")
+                else:
+                    print("Gpt Bot non ha fornito una risposta.\n")
+
+        except KeyboardInterrupt:
+            pass
+
+
+def main():
+    rclpy.init()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--api_key", type=str, default=os.getenv("OPENAI_Personal_Key"), help="OpenAI API key")
+    parser.add_argument("--model", type=str, default="gpt-5", help="OpenAI model to use")
+    parser.add_argument("--temperature",type=float,default=0.2,help="Sampling temperature" )
+    args = parser.parse_args()
+
+    node = GPTManager(api_key=args.api_key, model=args.model, temperature=args.temperature, with_tools=False)
+
+    try:
+        node.run()
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ gpt_control/tiago_arm_control_clean.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python3
+
+import math
+import time
+import rclpy
+from rclpy.node import Node
+from gazebo_msgs.srv import GetEntityState
+from geometry_msgs.msg import Twist
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from pymoveit2 import MoveIt2
+from linkattacher_msgs.srv import AttachLink, DetachLink
+
+
+def yaw_from_quaternion(q):
+    siny_cosp = 2.0 * (q.w * q.z + q.x * q.y)
+    cosy_cosp = 1.0 - 2.0 * (q.y * q.y + q.z * q.z)
+    return math.atan2(siny_cosp, cosy_cosp)
+
+
+class TiagoControl(Node):
+    def __init__(self):
+        super().__init__("tiago_control_node")
+
+        # Crea un client per il ros service fornito da gazebo per ottenere lo stato di un'entità nella simulazione
+        self.get_entity_client = self.create_client(GetEntityState, "/gazebo/get_entity_state")
+        # Crea un publisher per il topic /cmd_vel per controllare il movimento della base del robot
+        self.cmd_vel_pub = self.create_publisher(Twist, "/cmd_vel", 10)
+        # Crea un publisher per il topic del controller del gripper
+        self.gripper_pub = self.create_publisher(JointTrajectory, "/gripper_controller/joint_trajectory", 10)
+        # Crea i client per i ros service di attacco e distacco dei link per attaccarre oggetti al gripper del robot
+        self.attach_client = self.create_client(AttachLink, "/ATTACHLINK")
+        self.detach_client = self.create_client(DetachLink, "/DETACHLINK")
+        # Inizializza MoveIt2 per il controllo del braccio del robot
+        self.moveit2 = MoveIt2(node=self, joint_names=["arm_1_joint", "arm_2_joint", "arm_3_joint", "arm_4_joint", "arm_5_joint", "arm_6_joint", "arm_7_joint"], base_link_name="base_footprint", end_effector_name="gripper_grasping_frame", group_name="arm")
+        #self.moveit2 = MoveIt2(node=self, joint_names=["arm_1_joint", "arm_2_joint", "arm_3_joint", "arm_4_joint", "arm_5_joint", "arm_6_joint", "arm_7_joint"], base_link_name="base_footprint", end_effector_name="gripper_link", group_name="arm")
+        while not self.get_entity_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Waiting for /gazebo/get_entity_state...")
+        self.get_logger().info("Gazebo entity service available.")
+
+        self.get_logger().info("Tiago control node initialized.")
+
+
+
+    def get_entity_position(self, name: str):
+        req = GetEntityState.Request()
+        req.name = name
+        req.reference_frame = "world"
+        future = self.get_entity_client.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        pose = future.result().state.pose
+        return pose
+
+    def get_table_position(self):
+        pose = self.get_entity_position("table")
+        self.get_logger().info(f"Table @ x={pose.position.x:.3f}, y={pose.position.y:.3f}")
+        return pose.position.x, pose.position.y
+
+    def get_object_position_in_base(self, object_name: str):
+        obj_pose = self.get_entity_position(object_name)
+        robot_pose = self.get_entity_position("tiago")
+
+        dx = obj_pose.position.x - robot_pose.position.x
+        dy = obj_pose.position.y - robot_pose.position.y
+        dz = obj_pose.position.z - robot_pose.position.z
+
+        yaw = yaw_from_quaternion(robot_pose.orientation)
+        cos_y = math.cos(yaw)
+        sin_y = math.sin(yaw)
+
+        x_b = cos_y * dx + sin_y * dy
+        y_b = -sin_y * dx + cos_y * dy
+        z_b = dz
+
+        self.get_logger().info(f"{object_name} in base_footprint x={x_b:.3f}, y={y_b:.3f}, z={z_b:.3f}")
+        return x_b, y_b, z_b
+
+    def get_ee_pose(self):
+        ee = self.moveit2.compute_fk()
+        return ee.pose if hasattr(ee, "pose") else ee
+
+
+    """
+    def move_towards_table(self, table_x, table_y, distance_from_table=1.0):
+        robot_x, robot_y = 0.0, 0.0
+        dx = table_x - robot_x
+        dy = table_y - robot_y
+        distance = math.sqrt(dx * dx + dy * dy)
+        target_distance = distance - distance_from_table
+        if target_distance <= 0.5:
+            self.get_logger().info("Already close enough to table.")
+            return
+        angle = math.atan2(dy, dx)
+        self.get_logger().info(f"Move to table: dist={distance:.2f} m, angle={math.degrees(angle):.1f}°")
+
+        twist = Twist()
+        twist.angular.z = 0.3 if angle > 0.0 else -0.3
+        rot_time = abs(angle) / 0.3
+        start = time.time()
+        while time.time() - start < rot_time:
+            self.cmd_vel_pub.publish(twist)
+            time.sleep(0.1)
+        twist.angular.z = 0.0
+        self.cmd_vel_pub.publish(twist)
+        time.sleep(0.5)
+
+        twist.linear.x = 0.2
+        move_time = target_distance / 0.2
+        start = time.time()
+        while time.time() - start < move_time:
+            self.cmd_vel_pub.publish(twist)
+            time.sleep(0.1)
+        twist.linear.x = 0.0
+        self.cmd_vel_pub.publish(twist)
+        self.get_logger().info("Base positioned in front of table.")
+    """
+
+    def move_towards_table(self, table_x, table_y, distance_from_table=1.0):
+        robot_x, robot_y = 0.0, 0.0
+        dx = table_x - robot_x
+        dy = table_y - robot_y
+        distance = math.sqrt(dx * dx + dy * dy)
+        target_distance = distance - distance_from_table
+        if target_distance <= 0.5:
+            self.get_logger().info("Already close enough to table.")
+            return
+        angle = math.atan2(dy, dx)
+        self.get_logger().info(f"Move to table: dist={distance:.2f} m, angle={math.degrees(angle):.1f}°")
+        self.rotate_angle(angle, angular_speed=0.3)
+        time.sleep(0.5)
+        self.move_distance(distance=target_distance, speed=0.2)
+        self.get_logger().info("Base positioned in front of table.")
+
+
+    def move_distance(self, distance=0.4, speed=0.2):
+        """Move the base by `distance` meters using /cmd_vel."""
+        twist = Twist()
+        direction = 1.0 if distance >= 0.0 else -1.0
+        vel = abs(speed) * direction
+
+        twist.linear.x = vel
+        duration = abs(distance) / abs(speed)
+        start = self.get_clock().now().nanoseconds / 1e9
+
+        while (self.get_clock().now().nanoseconds / 1e9 - start) < duration:
+            self.cmd_vel_pub.publish(twist)
+            time.sleep(0.1)
+
+        twist.linear.x = 0.0
+        self.cmd_vel_pub.publish(twist)
+
+    def rotate_angle(self, angle, angular_speed=0.3):
+        twist = Twist()
+        direction = 1.0 if angle >= 0.0 else -1.0
+        twist.angular.z = abs(angular_speed) * direction
+        duration = abs(angle) / abs(angular_speed)
+        start = self.get_clock().now().nanoseconds / 1e9
+        while (self.get_clock().now().nanoseconds / 1e9 - start) < duration:
+            self.cmd_vel_pub.publish(twist)
+            time.sleep(0.1)
+        twist.angular.z = 0.0
+        self.cmd_vel_pub.publish(twist)
+
+
+    def open_gripper(self):
+        traj = JointTrajectory()
+        traj.joint_names = ["gripper_left_finger_joint", "gripper_right_finger_joint"]
+        pt = JointTrajectoryPoint()
+        pt.positions = [0.044, 0.044]
+        pt.time_from_start.sec = 1
+        traj.points = [pt]
+        self.gripper_pub.publish(traj)
+        time.sleep(2.0)
+
+    def close_gripper(self):
+        traj = JointTrajectory()
+        traj.joint_names = ["gripper_left_finger_joint", "gripper_right_finger_joint"]
+        pt = JointTrajectoryPoint()
+        pt.positions = [0.022, 0.022]
+        pt.time_from_start.sec = 1
+        traj.points = [pt]
+        self.gripper_pub.publish(traj)
+        time.sleep(2.0)
+
+
+    def move_gripper(self, x, y, z, quat_xyzw=(0.0075238, -0.027324, -0.0015946, 0.9997)):
+        self.get_logger().info(f"Move gripper to x={x:.3f}, y={y:.3f}, z={z:.3f}")
+        self.moveit2.move_to_pose(position=[x, y, z], quat_xyzw=list(quat_xyzw), cartesian=True)
+        self.moveit2.wait_until_executed()
+
+
+    def move_arm(self, joint_positions):  #in radians
+        self.get_logger().info(f"Moving arm to joints: {joint_positions}")
+        
+        if len(joint_positions) != 7:
+            self.get_logger().error("move_arm expects exactly 7 joint positions")
+            return
+
+        # Joint limits
+        # arm_1: [0.0, 2.748] (0 to 157.5 deg)
+        # arm_2: [-1.570, 1.090] (-90 to 62.5 deg)
+        # arm_3: [-3.534, 1.570] (-202.5 to 90 deg)
+        # arm_4: [-0.392, 2.356] (-22.5 to 135 deg)
+        # arm_5: [-2.094, 2.094] (-120 to 120 deg)
+        # arm_6: [-1.570, 1.570] (-90 to 90 deg)
+        # arm_7: [-2.094, 2.094] (-120 to 120 deg)
+        
+        limits = [
+            (0.0, 2.748),      # arm_1
+            (-1.570, 1.090),   # arm_2
+            (-3.534, 1.570),   # arm_3
+            (-0.392, 2.356),   # arm_4
+            (-2.094, 2.094),   # arm_5
+            (-1.570, 1.570),   # arm_6
+            (-2.094, 2.094)    # arm_7
+        ]
+
+        for i, (pos, (low, high)) in enumerate(zip(joint_positions, limits)):
+            if not (low <= pos <= high):
+                self.get_logger().error(f"Joint arm_{i+1}_joint value {pos:.3f} is out of limits [{low:.3f}, {high:.3f}]")
+                return
+
+        # Set the joint goal
+        self.moveit2.move_to_configuration(joint_positions=joint_positions,joint_names=["arm_1_joint", "arm_2_joint", "arm_3_joint", "arm_4_joint", "arm_5_joint", "arm_6_joint", "arm_7_joint"])
+        self.moveit2.wait_until_executed()
+        self.get_logger().info("Arm movement finished.")
+
+
+    def attach_object(self, model1="tiago", link1="arm_6_link", model2="cocacola", link2="link"):
+        req = AttachLink.Request()
+        req.model1_name = model1
+        req.link1_name = link1
+        req.model2_name = model2
+        req.link2_name = link2
+        future = self.attach_client.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        self.get_logger().info("AttachLink called.")
+
+
+    def detach_object(self, model1="tiago", link1="arm_6_link", model2="cocacola", link2="link"):
+        req = DetachLink.Request()
+        req.model1_name = model1
+        req.link1_name = link1
+        req.model2_name = model2
+        req.link2_name = link2
+        future = self.detach_client.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        self.get_logger().info("DetachLink called.")
+
+    def grasp_object_by_name_front(self, object_name: str, approach_dist: float = 0.30, grasp_offset: float = 0.05, height_offset: float = 0.0):
+        obj_x, obj_y, obj_z = self.get_object_position_in_base(object_name)
+        ee_pose = self.get_ee_pose()
+        ee_quat = [ee_pose.orientation.x, ee_pose.orientation.y, ee_pose.orientation.z, ee_pose.orientation.w]
+        self.get_logger().info(f"EE @ x={ee_pose.position.x:.3f}, y={ee_pose.position.y:.3f}, z={ee_pose.position.z:.3f}")
+
+        self.open_gripper()
+
+        direction = 1.0 if obj_x > 0.0 else -1.0
+        pre_x = obj_x - direction * approach_dist
+        grasp_x = obj_x - direction * grasp_offset
+        target_z = obj_z + height_offset
+
+        self.move_gripper(pre_x, obj_y, target_z, ee_quat)
+        self.move_gripper(grasp_x, obj_y, target_z+0.10, ee_quat)
+        self.attach_object(model2=object_name, link2="link")
+        self.move_gripper(pre_x, obj_y, target_z+0.25, ee_quat)
+        self.get_logger().info("Front grasp sequence finished.")
+        #self.move_gripper(grasp_x+0.2, obj_y, target_z+0.25, ee_quat)
+
+
+
+    def run(self):
+        time.sleep(2.0)
+        table_pose = self.get_object_position_in_base("table")
+        self.move_gripper(0.83578, -0.022147, 0.94412)
+        self.move_towards_table(table_pose[0], table_pose[1], distance_from_table=0.8)
+
+        self.grasp_object_by_name_front("cocacola")
+        self.detach_object(model2="cocacola", link2="link")
+
+        time.sleep(1.0)
+        self.move_gripper(0.53578, -0.022147, 0.94412)
+        self.move_gripper(0.53578, -0.022147, 0.94412)
+        self.rotate_angle(angle=math.radians(90), angular_speed=0.5)
+        self.move_distance(distance=0.3, speed=0.2)
+        self.rotate_angle(angle=math.radians(-90), angular_speed=0.5)
+        self.grasp_object_by_name_front("biscuits_pack")
+
+        self.detach_object(model2="biscuits_pack", link2="link")
+
+
+        
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TiagoControl()
+    try:
+        node.run()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
